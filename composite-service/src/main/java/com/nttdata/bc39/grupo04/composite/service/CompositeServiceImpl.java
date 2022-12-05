cbda0dfb13b041593848aaf7fb07dad027379e79
Nelson Jaimes Gonzales <nelsonjaimesgonzales@gmail.com>
2022-12-05 09:39:50 -0500

    update composite-service

--- composite-service/src/main/java/com/nttdata/bc39/grupo04/composite/service/CompositeServiceImpl.java
+++ composite-service/src/main/java/com/nttdata/bc39/grupo04/composite/service/CompositeServiceImpl.java
@@ -17,10 +17,6 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import javax.swing.text.Utilities;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
 
